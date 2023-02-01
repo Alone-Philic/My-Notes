cd9532de3bf9a66fd98991b53941d349cf7ea11a
Akhil Mahesh <109812106+Alone-Philic@users.noreply.github.com>
2023-02-01 14:44:09 +0530

    Add files via upload

---
+++ C Programming/Hello World.c
@@ -0,0 +1,17 @@
+/*
+ ============================================================================
+ Name        : Sample.c
+ Author      : AlonePhilic
+ Version     : 0.01
+ Copyright   : Your copyright notice
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+	printf("!!!Hello World!!!"); /* prints !!!Hello World!!! */
+	return EXIT_SUCCESS;
+}
