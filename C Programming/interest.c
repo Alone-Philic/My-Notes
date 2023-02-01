0997fa4ceed939356c8e79e3184929046f21d439
Akhil Mahesh <109812106+Alone-Philic@users.noreply.github.com>
2023-02-01 10:22:03 +0530

    Explained

--- C Programming/interest.c
+++ C Programming/interest.c
@@ -11,3 +11,9 @@ int main()
 	printf("%f\n",si);
 	return 0;
 }		
+
+//This code is a simple C program that calculates the simple interest (SI) of an investment.//
+//The program takes three inputs from the user: the principal amount (p), the number of years (n),//
+//and the interest rate (r). It then calculates the simple interest using the formula SI = p * n * r / 100 and prints the result.//
+//Note that the scanf statement uses the format specifier %d for the first two variables (p and n) because they are integers,//
+//and %f for the last variable (r) because it is a floating-point number.//
