73880d78a9233272deddd37e5d1114cc67d524ed
Akhil Mahesh <109812106+Alone-Philic@users.noreply.github.com>
2023-02-01 15:35:35 +0530

    Added Main Notes

---
+++ C Programming/Class/Enter a Number.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main() 
+{ int a;
+  printf("\n Enter a number :");
+  scanf("%d", &a);
+  printf("\n\n You've entered %d",a);
+  return 0;
+}
---
+++ C Programming/Class/hello.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+
+int main() {
+  printf("Welcome \v to the world \v of \t C Programming");
+  return 0;
+}
